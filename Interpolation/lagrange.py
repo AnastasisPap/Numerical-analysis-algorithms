d51754cef66cd630c2a1f2d6ef44915499baef50
Anastasios Papapanagiotou <anastasis.papapanagiotou@gmail.com>
2022-02-15 17:04:59 +0200

    added lagrange interpolation

---
+++ Interpolation/lagrange.py
@@ -0,0 +1,30 @@
+from IPython.display import display
+import numpy as np
+import sympy as sp
+
+def lagrange(f, values, degree = 10):
+    p = 0
+    x = sp.symbols('x')
+
+    for i in range(degree):
+        temp = 1
+        for j in range(degree):
+            if j == i:
+                continue
+            
+            temp *= (x - values[j]) / (values[i] - values[j])
+        
+        p += temp * f[i]
+
+    return p
+
+def main():
+    x = [0, 1, 2, 3]
+    f = [1, 2, 9, 28]
+
+    res = lagrange(f, x, 3)
+    display(res)
+    
+
+if __name__ == '__main__':
+    main()
