2ebdec4edbec99e8dab4eaafcf95dca9890e2d0c
Anastasios Papapanagiotou <anastasis.papapanagiotou@gmail.com>
2022-02-22 15:59:57 +0200

    added eulers simple method for differential equations

---
+++ Differential Equations solver/simple_euler_method.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def simple_euler(f, h, t, s0):
+    n = len(t)
+    s = np.zeros(n)
+    s[0] = s0
+
+    for i in range(n - 1):
+        s[i + 1] = s[i] + h * f(t[i], s[i])
+    
+    return s
+
+
+def plot(t, s, F, h, i, axs):
+    i -= 1
+    idx = ((i % 3), (i % 2))
+    axs[idx].plot(t, s, 'b', label='Approximation')
+    axs[idx].plot(t, F(t), 'g', label='Real')
+    axs[idx].set_title(f'h = {h}')
+    axs[idx].grid()
+
+def main():
+    f = lambda t, s: np.exp(-t)
+    F = lambda t: -np.exp(-t)
+
+    fig, axs = plt.subplots(3, 2)
+    for i in range(1, 7):
+        h = 1/2**i
+        t = np.linspace(0, 1, int(1/h) + 1)
+
+        s = simple_euler(f, h, t, -1)
+
+        plot(t, s, F, h, i, axs)
+    
+    for ax in axs.flat:
+        ax.set(xlabel='t', ylabel='f(t)')
+    fig.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
